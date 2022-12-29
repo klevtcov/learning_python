1a1f965449281103950c809d208df69256535449
Sergey <37954897+klevtcov@users.noreply.github.com>
2022-12-29 17:29:20 +0300

    4/12

--- stepic_django/store-server/store/users/views.py
+++ stepic_django/store-server/store/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
+
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -27,6 +28,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегистрированны!')
             return HttpResponseRedirect(reverse('users:login'))    
     else:
         form = UserRegistrationForm()
@@ -34,7 +36,16 @@ def registration(request):
     return render(request, 'users/registration.html', context=context)
 
 def profile(request):
-    form = UserProfileForm() 
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user) 
     context = {'title': 'Store - Профиль', 'form': form}
     return render(request, 'users/profile.html', context=context)
 
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))    
