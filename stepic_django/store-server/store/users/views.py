626f60485df69274957e3f29b968cc59f0d5e4fd
Sergey <37954897+klevtcov@users.noreply.github.com>
2022-12-29 09:59:43 +0300

    4.11(17)/12

--- stepic_django/store-server/store/users/views.py
+++ stepic_django/store-server/store/users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -20,8 +20,16 @@ def login(request):
     else:
         form = UserLoginForm()
     context = {'form': form}
-    return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html', context=context)
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))    
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context=context)
 
