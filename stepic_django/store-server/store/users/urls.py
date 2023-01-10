bc1f403829973989916169a7e305666ef4940c00
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-01-10 14:36:29 +0300

    7.6(14)/12

--- stepic_django/store-server/store/users/urls.py
+++ stepic_django/store-server/store/users/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
-from users.views import login, registration, profile, logout
+from users.views import login, logout, UserRegistrationView, UserProfileView
+from django.contrib.auth.decorators import login_required
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registration, name='registration'),
-    path('profile/', profile, name='profile'),
+    # path('registration/', registration, name='registration'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
+    # path('profile/', profile, name='profile'),
+    path('profile/<int:pk>', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', logout, name='logout'),
 ]
 
--- stepic_django/store-server/store/users/views.py
+++ stepic_django/store-server/store/users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.views.generic.edit import CreateView, UpdateView
 
 from products.models import Basket
 
 # Create your views here.
+
+
 
 def login(request):
     if request.method == 'POST':
@@ -26,19 +29,48 @@ def login(request):
     context = {'form': form}
     return render(request, 'users/login.html', context=context)
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Вы успешно зарегистрированны!')
-            return HttpResponseRedirect(reverse('users:login'))    
-    else:
-        form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/registration.html', context=context)
 
-@login_required
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title']  = 'Store - Регистрация'
+        return context
+
+''' def registration(request):
+        if request.method == 'POST':
+            form = UserRegistrationForm(data=request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Вы успешно зарегистрированны!')
+                return HttpResponseRedirect(reverse('users:login'))    
+        else:
+            form = UserRegistrationForm()
+        context = {'form': form}
+        return render(request, 'users/registration.html', context=context)
+        '''
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title']  = 'Store - Личный кабинет'
+        context['baskets'] = Basket.objects.filter(user=self.object) 
+        return context
+
+'''@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -51,7 +83,6 @@ def profile(request):
     baskets = Basket.objects.filter(user=request.user)    
     # total_sum = sum(basket.sum() for basket in baskets)
     # total_quantity = sum(basket.quantity for basket in baskets)
-
     # total_sum = 0
     # total_quantity = 0
     # for basket in baskets:
@@ -66,6 +97,8 @@ def profile(request):
         # 'total_quantity': total_quantity
         }
     return render(request, 'users/profile.html', context=context)
+'''
+
 
 def logout(request):
     auth.logout(request)
