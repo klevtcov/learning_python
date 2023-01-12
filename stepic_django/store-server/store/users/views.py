11b03c4ea948cd31e6ff3bc965406e4076c8ba83
Sergey <37954897+klevtcov@users.noreply.github.com>
2023-01-12 10:09:04 +0300

    7/12

--- stepic_django/store-server/store/users/views.py
+++ stepic_django/store-server/store/users/views.py
@@ -22,6 +22,54 @@ class UserLoginView(TitleMixin, LoginView):
     form_class = UserLoginForm
     title = 'Store - Авторизация'
 
+
+class UserRegistrationView(SuccessMessageMixin, TitleMixin, CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+    success_message = 'Вы успешно зарегистрированны!'
+    title = 'Store - Регистрация'
+
+
+class UserProfileView(TitleMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
+    title = 'Store - Личный кабинет'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        # context['title']  = 'Store - Личный кабинет'
+        context['baskets'] = Basket.objects.filter(user=self.object) 
+        return context
+
+
+class EmailVerificationView(TitleMixin, TemplateView):
+    title = 'Store - Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
+    
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verification = EmailVerification.objects.filter(user=user, code=code)
+        if email_verification.exists() and not email_verification.first().is_expired():
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(request)
+        else:
+            return HttpResponseRedirect(reverse('index'))
+
+
+
+
+
+
+
 '''def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
